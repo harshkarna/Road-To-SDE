a06919f1ccda2457a43764ef765fdc902ca12d8a
Harsh Karna <harsh.karna@cdk.com>
2022-02-27 23:02:00 +0530

    adding recursion programs

---
+++ Recusion/Number_of_digits.java
@@ -0,0 +1,39 @@
+//Number of Digits
+//
+//        Given the code to find out and return the number of digits present in a number recursively.
+//        But it contains few bugs, that you need to rectify such that all the test cases should pass.
+//        Input Format :
+//        Integer n
+//        Output Format :
+//        Count of digits
+//        Constraints :
+//        1 <= n <= 10^6
+//        Sample Input 1 :
+//        156
+//        Sample Output 1 :
+//        3
+//        Sample Input 2 :
+//        7
+//        Sample Output 2 :
+//        1
+
+package com.Recusion;
+
+public class Number_of_digits {
+
+    public static int no_of_digits(int n){
+        if(n==0){
+            return 0;
+        }
+
+        int smallcase=no_of_digits(n/10);
+        return smallcase+1;
+
+    }
+
+    public static void main(String[] args) {
+        int n =156;
+        int anw =no_of_digits(n);
+        System.out.println(anw);
+    }
+}
