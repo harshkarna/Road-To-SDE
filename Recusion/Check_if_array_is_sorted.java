f407b577cb091ba9fb70b4c80fb872e6261c7b68
Harsh Karna <harshkarna4@gmail.com>
2022-03-02 23:39:30 +0530

    adding  recusrion progranms

---
+++ Recusion/Check_if_array_is_sorted.java
@@ -0,0 +1,52 @@
+package com.Recusion;
+
+public class Check_if_array_is_sorted {
+
+    public static boolean check_sorted_1(int arr[]) {
+        if (arr.length <= 1) {
+            return true;
+        }
+
+        int small_case_arr[] = new int[arr.length - 1];
+        for (int i = 1; i < arr.length; i++) {
+            small_case_arr[i - 1] = arr[i];
+        }
+
+        boolean smallcase=check_sorted_1(small_case_arr);
+        if(!smallcase){
+            return false;
+        }
+        else if(arr[0]<=arr[1]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public static boolean check_sorted_2(int arr[]) {
+        if (arr.length <= 1) {
+            return true;
+        }
+
+        if(arr[0]>arr[1]){
+            return false;
+        }
+
+        int small_case_arr[] = new int[arr.length - 1];
+        for (int i = 1; i < arr.length; i++) {
+            small_case_arr[i - 1] = arr[i];
+        }
+        boolean smallcase=check_sorted_1(small_case_arr);
+        return smallcase;
+
+    }
+
+
+    public static void main(String[] args) {
+        int arr[]={2,1,3,4,5};
+        System.out.println( check_sorted_2(arr));
+
+    }
+}
---
+++ Recusion/Sum_of_array.java
@@ -0,0 +1,48 @@
+//
+//Sum of Array
+//
+//        Given an array of length N, you need to find and return the sum of all elements of the array.
+//        Do this recursively.
+//        Input Format :
+//        Line 1 : An Integer N i.e. size of array
+//        Line 2 : N integers which are elements of the array, separated by spaces
+//        Output Format :
+//        Sum
+//        Constraints :
+//        1 <= N <= 10^3
+//        Sample Input 1 :
+//        3
+//        9 8 9
+//        Sample Output 1 :
+//        26
+//        Sample Input 2 :
+//        3
+//        4 2 1
+//        Sample Output 2 :
+//        7
+package com.Recusion;
+
+public class Sum_of_array {
+
+    public static int sum_array(int arr[]){
+        if(arr.length==1){
+            return arr[arr.length-1];
+        }
+
+        int small_case_arr[]= new int[arr.length-1];
+        for(int i=1;i<arr.length;i++){
+            small_case_arr[i-1]=arr[i];
+        }
+        int smallcase=sum_array(small_case_arr);
+        return arr[0]+smallcase;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int arr[]={4,2,1};
+        System.out.println(sum_array(arr));
+
+    }
+}
