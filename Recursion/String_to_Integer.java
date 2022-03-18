3e774db253ef1034145a87e08c5e08c9b513cf3b
Harsh Karna <harshkarna4@gmail.com>
2022-03-18 15:02:53 +0530

    adding str_to_int recursive program

--- Recursion/String_to_Integer.java
+++ Recursion/String_to_Integer.java
@@ -1,7 +1,8 @@
 //String to Integer
 //
 //        Write a recursive function to convert a given string into the number it represents.
-//        That is input will be a numeric string that contains only numbers, you need to convert the string into corresponding integer and return the answer.
+//        That is input will be a numeric string that contains only numbers, you need to convert
+//        the string into corresponding integer and return the answer.
 //        Input format :
 //        Numeric string S (string, Eg. "1234")
 //        Output format :
@@ -18,17 +19,24 @@
 //        Sample Output 2 :
 //        12567
 
-//package com.Recursion;
-//
-//public class String_to_Integer {
-//
-//    public static int Str_to_int(String str){
-//
-//    }
-//
-//    public static void main(String[] args) {
-//      String str="12345";
-//      System.out.println(Str_to_int(str));
-//
-//    }
-//}
+package com.Recursion;
+
+public class String_to_Integer {
+
+    public static int str_to_int(String str){
+        if(str.length()==0){
+            return 0;
+        }
+
+        double smallcase =str_to_int(str.substring(1));
+        double ans=str.charAt(0)-'0';
+        return (int) (ans * Math.pow(10,str.substring(1).length()) + smallcase);
+
+    }
+
+    public static void main(String[] args) {
+      String str="123479304";
+      System.out.println(str_to_int(str));
+
+    }
+}
