e58dc07c4857de472cc128618987cbf8d11062fe
Harsh Karna <harshkarna4@gmail.com>
2022-07-27 22:21:25 +0530

    started Linked list 2

---
+++ Linked_List/Insert_node_Recursively_in_LL.java
@@ -0,0 +1,54 @@
+package com.Linked_List;
+
+public class Insert_node_Recursively_in_LL {
+
+    public static Node <Integer> createLinkedList() {
+        Node<Integer> n1=new Node<>(10);
+        Node<Integer> n2=new Node<>(20);
+        Node<Integer> n3=new Node<>(30);
+        Node<Integer> n4=new Node<>(40);
+        n1.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+        return n1;
+
+    }
+
+    public static void print_recursive(Node <Integer> head){
+        if(head==null){
+            return;
+        }
+        System.out.print(head.data+" ");
+        print_recursive(head.next);
+
+    }
+
+
+    public static Node <Integer> insert_recursively(Node <Integer> head, int  data, int index){
+        if(head==null && index > 0){
+            return head;
+        }
+        //Base cas
+        if(index==0){
+            Node <Integer> new_head= new Node<>(data);
+            new_head.next=head;
+            return new_head;
+        }
+        else{
+           Node <Integer> smallerhead= insert_recursively(head.next,data, index-1);
+           head.next=smallerhead;
+
+           return head;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Node <Integer> head=createLinkedList();
+        head=insert_recursively(head,3,9);
+        print_recursive(head);
+
+
+    }
+}
---
+++ Linked_List/Print_LL_Recursively.java
@@ -0,0 +1,35 @@
+package com.Linked_List;
+
+public class Print_LL_Recursively {
+
+
+    public static Node <Integer> createLinkedList() {
+        Node<Integer> n1=new Node<>(10);
+        Node<Integer> n2=new Node<>(20);
+        Node<Integer> n3=new Node<>(30);
+        Node<Integer> n4=new Node<>(40);
+        n1.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+        return n1;
+
+    }
+
+
+    public static void print_recursive(Node <Integer> head){
+        if(head==null){
+            return;
+        }
+        System.out.print(head.data+" ");
+        print_recursive(head.next);
+
+    }
+
+
+    public static void main(String[] args) {
+        Node <Integer> head=createLinkedList();
+        print_recursive(head);
+
+
+    }
+}
