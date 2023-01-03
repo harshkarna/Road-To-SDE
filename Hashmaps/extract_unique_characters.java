a259d4f14eb703eb292760a3df0bfa2e77e8aedd
Harsh Karna <harshkarna4@gmail.com>
2023-01-03 11:24:18 +0530

    adding questions to hashmaps cs

---
+++ Hashmaps/extract_unique_characters.java
@@ -0,0 +1,49 @@
+//Extract Unique characters
+//
+//        Given a string S, you need to remove all the duplicates. T
+//        hat means, the output string should contain each character only once. The respective order of characters should remain same, as in the input string.
+//        Input format:
+//        The first and only line of input contains a string, that denotes the value of S.
+//        Output format :
+//        The first and only line of output contains the updated string, as described in the task.
+//        Constraints :
+//        0 <= Length of S <= 10^8
+//        Time Limit: 1 sec
+//        Sample Input 1 :
+//        ababacd
+//        Sample Output 1 :
+//        abcd
+//        Sample Input 2 :
+//        abcde
+//        Sample Output 2 :
+//        abcde
+
+package com.Hashmaps;
+
+import java.util.HashMap;
+
+public class extract_unique_characters {
+
+    private static String getuniquecharacters(String str) {
+        
+        HashMap<Character,Boolean> map=new HashMap<>();
+        String ans="";
+
+        for(int i=0;i <str.length();i++){
+            char s=str.charAt(i);
+            if(!map.containsKey(s)){
+                map.put(s,true);
+                ans=ans + s;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String str="abcde";
+        System.out.println(getuniquecharacters(str));
+
+    }
+
+
+}
